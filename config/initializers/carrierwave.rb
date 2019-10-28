819e8782246736e022f9a5923717ade3f3b7c18d
Oliver Valls <oliver.vh@coditramuntana.com>
2019-10-28 16:15:56 +0100

    Set amazon S3 host to Paris.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,7 +19,7 @@ if Rails.application.secrets.aws_access_key_id.present?
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'eu-west-3',                                       # optional, defaults to 'us-east-1'
-      host:                  's3.eu-central-1.amazonaws.com',                                  # optional, defaults to nil
+      host:                  's3.eu-west-3.amazonaws.com',                                  # optional, defaults to nil
     }
     config.fog_directory  = 'meta-decidim-production'                                 # required
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
