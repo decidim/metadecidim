1d7e5e38ded9c13770eae95f5f3aa25f30f00ad5
Andrés Pereira de Lucena <andreslucena@gmail.com>
2021-07-23 09:14:44 +0200

    Test unconfirmed access

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Authentication', type: :system do
+  let(:organization) { create(:organization) }
+
+  before do
+    switch_to_host(organization.host)
+    visit decidim.root_path
+  end
+
+  describe 'Sign Up' do
+    context 'when using email and password' do
+      it 'ask confirmations on new Users' do
+        find('.sign-up-link').click
+
+        within '.new_user' do
+          fill_in :registration_user_email, with: 'user@example.org'
+          fill_in :registration_user_name, with: 'Responsible Citizen'
+          fill_in :registration_user_nickname, with: 'responsible'
+          fill_in :registration_user_password, with: 'DfyvHn425mYAy2HL'
+          fill_in :registration_user_password_confirmation, with: 'DfyvHn425mYAy2HL'
+          check :registration_user_tos_agreement
+          check :registration_user_newsletter
+          find('*[type=submit]').click
+        end
+
+        expect(page).to have_content('confirmation link')
+      end
+    end
+  end
+end
