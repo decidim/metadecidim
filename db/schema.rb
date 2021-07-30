6bb2d6a72206141ed83dc2b644a2dd94a24daa1f
Oriol Gual <oriol@codegram.com>
2021-07-30 10:11:51 +0200

    Upgrade to Decidim 0.24.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_080311) do
+ActiveRecord::Schema.define(version: 2021_07_30_080110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
