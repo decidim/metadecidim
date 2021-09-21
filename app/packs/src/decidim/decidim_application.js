30b338ef36c201c93c5b9ef968b320b2004d434d
Leonardo Diez <leiodd@gmail.com>
2021-09-21 10:14:32 +0200

    chore: webpacker migration

---
+++ app/packs/src/decidim/decidim_application.js
@@ -0,0 +1,6 @@
+// This file is compiled inside Decidim core pack. Code can be added here and will be executed
+// as part of that pack
+
+// Load images
+require.context("../../images", true)
+require.context("../../fonts", true)
