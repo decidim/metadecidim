80a64c16f414895b91a532013ed1a325e9538469
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2022-09-19 16:54:51 +0200

    Add cookies and LocalStorage configuration (#106)
    * Add cookies configuration for YouTube and Decidim
    
    * Add remember_user_token devise cookie
    
    * Add LocalStorage type
    
    * Add emojiPicker LocalStorage configuration
    
    * Add GraphiQL LocalStorage configuration
    
    * Add Meet Jitsi LocalStorage configuration
    
    * Renaming cookies consent to items/data consent

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -339,12 +339,12 @@ Decidim.configure do |config|
   # set cookies.
   config.consent_cookie_name = Rails.application.secrets.decidim[:consent_cookie_name] if Rails.application.secrets.decidim[:consent_cookie_name].present?
 
-  # Defines cookie consent categories and cookies.
+  # Defines data consent categories and items (aka cookies consent).
   # config.consent_categories = [
   #   {
   #     slug: "essential",
   #     mandatory: true,
-  #     cookies: [
+  #     items: [
   #       {
   #         type: "cookie",
   #         name: "_session_id"
@@ -485,3 +485,164 @@ Rails.application.config.i18n.default_locale = Decidim.default_locale
 
 # Inform Decidim about the assets folder
 Decidim.register_assets_path File.expand_path("app/packs", Rails.application.root)
+
+Decidim.configure do |config|
+  config.consent_categories = [
+    {
+      slug: "essential",
+      mandatory: true,
+      items: [
+        {
+          type: "cookie",
+          name: "_session_id"
+        },
+        {
+          type: "cookie",
+          name: Decidim.consent_cookie_name
+        },
+        {
+          type: "cookie",
+          name: "remember_user_token"
+        }
+      ]
+    },
+    {
+      slug: "preferences",
+      mandatory: false,
+      items: [
+        {
+          type: "localstorage",
+          name: "emojiPicker-recent"
+        },
+        {
+          type: "localstorage",
+          name: "graphiql:docExplorerOpen"
+        },
+        {
+          type: "localstorage",
+          name: "graphiql:editorFlex"
+        },
+        {
+          type: "localstorage",
+          name: "graphiql:historyPaneOpen"
+        },
+        {
+          type: "localstorage",
+          name: "graphiql:queries"
+        },
+        {
+          type: "localstorage",
+          name: "graphiql:query"
+        },
+        {
+          type: "localstorage",
+          name: "graphiql:tabState"
+        },
+        {
+          type: "cookie",
+          name: "YSC"
+        },
+        {
+          type: "cookie",
+          name: "VISITOR_INFO1_LIVE"
+        },
+        {
+          type: "cookie",
+          name: "PREF"
+        },
+        {
+          type: "cookie",
+          name: "CONSENT"
+        },
+        {
+          type: "localstorage",
+          name: "language"
+        },
+        {
+          type: "localstorage",
+          name: "features/base/known-domains"
+        },
+        {
+          type: "localstorage",
+          name: "features/base/settings"
+        },
+        {
+          type: "localstorage",
+          name: "features/calendar-sync"
+        },
+        {
+          type: "localstorage",
+          name: "features/dropbox"
+        },
+        {
+          type: "localstorage",
+          name: "features/prejoin"
+        },
+        {
+          type: "localstorage",
+          name: "features/recent-list"
+        },
+        {
+          type: "localstorage",
+          name: "features/video-quality-persistent-storage"
+        },
+        {
+          type: "localstorage",
+          name: "features/virtual-background"
+        },
+        {
+          type: "localstorage",
+          name: "endpointID"
+        },
+        {
+          type: "localstorage",
+          name: "callStatsUserName"
+        },
+        {
+          type: "localstorage",
+          name: "endpointID"
+        },
+        {
+          type: "localstorage",
+          name: "callStatsUserName"
+        },
+        {
+          type: "localstorage",
+          name: "endpointID"
+        },
+      ]
+    },
+    {
+      slug: "analytics",
+      mandatory: false,
+      items: [
+        {
+          type: "cookie",
+          name: "VISITOR_INFO1_LIVE"
+        },
+        {
+          type: "localstorage",
+          name: "amp_"
+        },
+        {
+          type: "localstorage",
+          name: "amplitude_unsent"
+        },
+        {
+          type: "localstorage",
+          name: "amplitude_unsent_identify"
+        },
+      ]
+    },
+    {
+      slug: "marketing",
+      mandatory: false,
+      items: [
+        {
+          type: "cookie",
+          name: "VISITOR_INFO1_LIVE"
+        }
+      ]
+    }
+  ]
+end
