803056f5222944321bb2cd2f013af80682435fe5
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-10-07 13:12:05 +0200

    Change seeds to have the menu on development

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,17 @@ if ENV["HEROKU_APP_NAME"].present?
   ENV["DECIDIM_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
   ENV["SEED"] = true
 end
+
 Decidim.seed!
+
+puts "Making local configurations changes..."
+
+puts "-- Changing the organization's name to Metadecidim"
+Decidim::Organization.first.update!(name: 'Metadecidim')
+
+puts "-- Changing the slugs and IDs to correspond with the ones from the menu"
+Decidim::ParticipatoryProcess.first.update!(slug: 'Welcome')
+Decidim::ParticipatoryProcessGroup.first.update!(id: 76)
+Decidim::Assembly.first.update!(slug: 'our-governance')
+Decidim::ParticipatoryProcess.last.update!(slug: "news")
+Decidim::ParticipatoryProcess.last.components.where(manifest_name: 'blogs').first.update!(id: 1719, name: { en: 'news' })
