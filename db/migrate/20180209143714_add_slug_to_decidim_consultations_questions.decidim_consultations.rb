8fd20ab0a9afb6473fad1f1a5a3d700eb7912f5c
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-12-01 18:09:22 +0200

    Add migrations

--- db/migrate/20180209143714_add_slug_to_decidim_consultations_questions.decidim_consultations.rb
+++ db/migrate/20180209143714_add_slug_to_decidim_consultations_questions.decidim_consultations.rb
@@ -2,17 +2,21 @@
 # frozen_string_literal: true
 
 class AddSlugToDecidimConsultationsQuestions < ActiveRecord::Migration[5.1]
+  class Question < ApplicationRecord
+    self.table_name = :decidim_consultations_questions
+  end
+
   def change
     add_column :decidim_consultations_questions,
                :decidim_organization_id,
                :integer,
                index: {
-                 name: "index_decidim_questions_on_decidim_organization_id"
+                 name: 'index_decidim_questions_on_decidim_organization_id'
                }
 
     add_column :decidim_consultations_questions, :slug, :string
 
-    Decidim::Consultations::Question.find_each do |question|
+    Question.find_each do |question|
       question.decidim_organization_id = question.consultation.decidim_organization_id
       question.slug = "q-#{question.id}"
       question.save
