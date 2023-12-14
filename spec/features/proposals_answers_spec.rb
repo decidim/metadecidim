16cfd1d58b90eb8a12c15f4fcb43f5699e4482ce
Andrés Pereira de Lucena <andreslucena@gmail.com>
2023-12-14 10:34:45 +0100

    Fix specs

--- spec/features/proposals_answers_spec.rb
+++ spec/features/proposals_answers_spec.rb
@@ -4,61 +4,118 @@ require "rails_helper"
 require "decidim/proposals/test/factories"
 
 describe "Views the proposals answers overrides", type: :system do
-  let!(:accepted_proposal) { create :proposal, :accepted, component: }
-  let!(:evaluating_proposal) { create :proposal, :evaluating, component: }
+  let!(:finished_proposal) { create :proposal, :accepted, component:, title: { en: "This proposal has finished" } }
+  let!(:progressing_proposal) { create :proposal, :evaluating, component:, title: { en: "This proposal is in progress" } }
 
   include_context "with a component"
   let(:manifest_name) { "proposals" }
 
-  before do
-    visit_component
-  end
-
-  it "views the overrided colors" do
-    within "main" do
-      # Capybara understands rgba instead of hexadecimal colors
-      expect(page.find("span.label.finished")).to match_style("background-color" => "rgba(204, 190, 230, 1)")
-      expect(page.find("span.label.finished")).to match_style("color" => "rgba(102, 57, 186, 1)")
-      expect(page.find("span.label.progressing")).to match_style("background-color" => "rgba(196, 236, 208, 1)")
-      expect(page.find("span.label.progressing")).to match_style("color" => "rgba(22, 89, 46, 1)")
-    end
-  end
-
-  context "with the english locale" do
-    it "views the overrided strings" do
-      within "main" do
-        expect(page).to have_content("Finished")
-        expect(page).to have_content("Accepted / In progress")
-      end
-    end
-  end
-
-  context "with the catalan locale" do
+  describe "index" do
     before do
-      within_language_menu do
-        click_link "Català"
+      visit_component
+    end
+
+    it "views the overrided colors" do
+      within "main" do
+        # Capybara understands rgba instead of hexadecimal colors
+        expect(page.find("span.label.finished")).to match_style("background-color" => "rgba(246, 245, 252, 1)")
+        expect(page.find("span.label.finished")).to match_style("color" => "rgba(102, 57, 186, 1)")
+        expect(page.find("span.label.progressing")).to match_style("background-color" => "rgba(196, 236, 208, 1)")
+        expect(page.find("span.label.progressing")).to match_style("color" => "rgba(22, 89, 34, 1)")
       end
     end
 
-    it "views the overrided strings" do
-      within "main" do
-        expect(page).to have_content("Finalitzada")
-        expect(page).to have_content("Acceptada / En curs")
+    context "with the english locale" do
+      it "views the overrided strings" do
+        within "main" do
+          expect(page).to have_content("Finished")
+          expect(page).to have_content("Accepted / In progress")
+        end
+      end
+    end
+
+    context "with another locale" do
+      before do
+        within_language_menu do
+          click_link "Català"
+        end
+      end
+
+      it "views the overrided strings" do
+        within "main" do
+          expect(page).to have_content("Finalitzada")
+          expect(page).to have_content("Acceptada / En curs")
+        end
       end
     end
   end
 
-  context "with the spanish locale" do
+  describe "show" do
     before do
-      within_language_menu do
-        click_link "Castellano"
+      visit Decidim::EngineRouter.main_proxy(component).proposal_path(proposal)
+    end
+
+    context "with the finished proposal" do
+      let(:proposal) { finished_proposal }
+
+      it "views the overrided colors" do
+        within "main" do
+          # Capybara understands rgba instead of hexadecimal colors
+          expect(page.find("div.flash.finished")).to match_style("background-color" => "rgba(246, 245, 252, 1)")
+          expect(page.find("div.flash.finished")).to match_style("border-color" => "rgb(102, 57, 186)")
+        end
+      end
+
+      it "views the overrided strings" do
+        within "main div.flash.finished" do
+          expect(page).to have_content("This proposal has been implemented")
+        end
+      end
+
+      context "with another locale" do
+        before do
+          within_language_menu do
+            click_link "Castellano"
+          end
+        end
+
+        it "views the overrided strings" do
+          within "main div.flash.finished" do
+            expect(page).to have_content("Esta propuesta ha sido implementada")
+          end
+        end
       end
     end
 
-    it "views the overrided strings" do
-      within "main" do
-        expect(page).to have_content("Finalizada")
-        expect(page).to have_content("Aceptada / En curso")
+    context "with the progressing proposal" do
+      let(:proposal) { progressing_proposal }
+
+      it "views the overrided colors" do
+        within "main" do
+          # Capybara understands rgba instead of hexadecimal colors
+          expect(page.find("div.flash.progressing")).to match_style("background-color" => "rgba(196, 236, 208, 1)")
+          expect(page.find("div.flash.progressing")).to match_style("border-color" => "rgb(22, 89, 34)")
+        end
+      end
+
+      it "views the overrided strings" do
+        within "main div.flash.progressing" do
+          expect(page).to have_content("This proposal has been accepted")
+        end
+      end
+
+      context "with another locale" do
+        before do
+          within_language_menu do
+            click_link "Català"
+          end
+        end
+
+        it "views the overrided strings" do
+          within "main div.flash.progressing" do
+            expect(page).to have_content("Aquesta proposta ha sigut acceptada")
+          end
+        end
       end
     end
   end
