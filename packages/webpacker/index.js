cacb2734e1068c419e6e2dbfaea95a2ae42d5556
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-01-11 14:13:33 +0100

    Update decidim to release/0.28-stable branch

--- packages/webpacker/index.js
+++ packages/webpacker/index.js
@@ -1,6 +1,9 @@
-const webpacker = require("@rails/webpacker");
+const { generateWebpackConfig, ...restOpts } = require("shakapacker");
+const webpackConfig = generateWebpackConfig()
+
 const overrideConfig = require("./src/override-config");
 
-module.exports = Object.assign(webpacker, { // eslint-disable-line
-  webpackConfig: overrideConfig(webpacker.webpackConfig)
-});
+module.exports = {
+  webpackConfig: overrideConfig(webpackConfig),
+  ...restOpts
+};
