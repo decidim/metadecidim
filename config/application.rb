ecdee3a7cc5e7cd2748f19cbfae0d892daebfc49
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-07-12 11:30:25 +0200

    Use rails 5.1 defaults and use zeitwerk as autoloader

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ Bundler.require(*Rails.groups)
 module Metadecidim
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 5.1
+    config.autoloader = :zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
