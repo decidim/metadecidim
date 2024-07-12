82af36996bee42c6f309bf0cd71b2f6be2c05362
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2024-07-12 12:55:12 +0200

    Bump decidim from v0.28.0 to v0.28.1 (#128)
    * Bump decidim from v0.28.0 to v0.28.1
    
    * Remove the unnecessary packages directory
    
    Suggested in the code review
    
    * Bump tailwindcss from 3.3.5 to 3.4.1
    
    * Change application load_defaults from 5.1 to 6.0
    
    * Use rails 5.1 defaults and use zeitwerk as autoloader
    
    ---------
    
    Co-authored-by: Francisco Bolívar <francisco.bolivar@nazaries.com>

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Metadecidim
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.autoloader = :zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
