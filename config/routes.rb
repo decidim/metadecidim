c13c763d25f04889918f1ae83304c6b5204d888d
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-07-16 16:07:17 +0200

    Redirect the old slug to the new one

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  get "/assemblies/our-governance", to: redirect("/assemblies/the-association")
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
